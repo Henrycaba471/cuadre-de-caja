1d5f46f5f79136d3c6df8e05cfa4ed0893e58a8b
Henry Cañizares <henrycaba471@gmail.com>
2024-09-09 12:56:15 -0500

    se anexo transferencias validar datos

--- assets/js/transfers.js
+++ assets/js/transfers.js
@@ -9,13 +9,18 @@ document.addEventListener('DOMContentLoaded', (e) => {
     const btnCancel = document.getElementById('cancel');
 
     btnPrint.addEventListener('click', () =>{
-        console.log(`${entidadBamcaria.value} ${numeroCuenta.value} ${nombre.value} ${cedula.value} ${valorSoberanos.value}`);
+
+        if(entidadBamcaria.value === '' || numeroCuenta.value === '' || nombre.value === '' || cedula.value === '' || valorSoberanos.value === '') {
+            alert('Hay campos vacios');
+            return
+        }
 
         const contenido = `
         ------------------------------- <br>
         TRANSFERENCIAS A VENEZUELA <br>
         ------------------------------- <br>
         <br>
+        FECHA: ${new Date().toLocaleString()}<br>
         BANCO:${entidadBamcaria.value.toUpperCase()}<br>
         CUENTA N°:<br>
         ${numeroCuenta.value}<br>
@@ -25,16 +30,23 @@ document.addEventListener('DOMContentLoaded', (e) => {
         <br>
         SOBERANOS B$: ${valorSoberanos.value}<br>
         <br>
-        <br>
         ------------------------------------ <br>
         Representante: Jose T Garcia <br>
         WhatsApp: 3124824321 <br>
         ------------------------------------ <br>
+        <br>
+        <br>
+        FIRMA CAJERO:_______________________<br>
+        <br>
+        <br>
+
+        ...
         `
 
-        const windowPrint = window.open('', 'Impresion', 'width=500,height=600');
+        const windowPrint = window.open('', 'Impresion', 'width=600,height=600');
         windowPrint.document.write(contenido);
         windowPrint.document.close();
         windowPrint.print();
     });
+
 })
